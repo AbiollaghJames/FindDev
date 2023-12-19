26696d62ff49bd933ffd42f288beab6deaf16bc1
Addiowen <owenoscar18@gmail.com>
2023-12-19 23:58:38 +0300

    updated models and views

--- models/devs.js
+++ models/devs.js
@@ -1,31 +1,49 @@
 const mongoose = require("mongoose");
 
+// const devSchema = new mongoose.Schema({
+    // name: {
+    //     type: String,
+    //     required: true,
+    // },
+    // email: {
+    //     type: String,
+    //     required: true,
+    // },
+    // location: {
+    //     type: String,
+    //     required: true,
+    // },
+    // techStack: {
+    //     type: [String],
+    //     default: [],
+    // },
+    // social_media: {
+    //     github: String,
+    //     linked_in: String,
+    //     twitter: String,
+    // },
+    // project: {
+    //     title: String,
+    //     description: String,
+    //     url: String,
+    // },
+
+const projectSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    url: String,
+    });
+      
 const devSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-    },
-    location: {
-        type: String,
-        required: true,
-    },
-    techStack: {
-        type: [String],
-        default: [],
-    },
-    social_media: {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    location: String,
+    techstack: { type: [String], default: [] },
+    projects: [projectSchema],
+    socialMedia: {
         github: String,
-        linked_in: String,
+        linkedin: String,
         twitter: String,
-    },
-    project: {
-        title: String,
-        description: String,
-        url: String,
     },
 });
 
